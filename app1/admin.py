279cb81f9f3524757049bbe996ff06a59b0d522d
gerardorosales2222 <gerardorosales2222@gmail.com>
2024-06-23 16:47:25 -0300

    6 primeros modelso

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Titulo)
+admin.site.register(Materia)
+
+
+@admin.register(Profesor)
+class profesor(admin.ModelAdmin):
+    list_display = ('id', 'dni', 'nombre', 'apellido', 'tel', 'get_titulos')
+
+    def get_titulos(self, obj):
+        return ', '.join([titulo.nombre for titulo in obj.titulos.all()])
+
+@admin.register(Tipo_de_Carrera)
+class tipo_carrera(admin.ModelAdmin):
+    list_display = ('tipo', 'duracion')
+
+@admin.register(Carrera)
+class carrera(admin.ModelAdmin):
+    list_display = ('nombre', 'resolucion')
+    
+@admin.register(Alumno)
+class alumno(admin.ModelAdmin):
+    list_display = ('id', 'dni', 'nombre', 'apellido', 'tel', 'get_carreras')
+
+    def get_carreras(self, obj):
+        return ', '.join([Carrera.nombre for Carrera in obj.carreras.all()])
+    
+admin.site.site_header = 'IES'
+admin.site.index_title = 'Panel de control'
+admin.site.site_title = 'Administración del IES'
--- app1/models.py
+++ app1/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Título(models.Model):
+class Titulo(models.Model):
     nombre = models.CharField(max_length=8, null=False, verbose_name='Título')
     def __str__(self):
         return '%s'%(self.nombre)  
@@ -14,10 +14,54 @@ class Profesor(models.Model):
     nombre = models.CharField(max_length=20, null=False, verbose_name='Nombre')
     apellido = models.CharField(max_length=20, null=False, verbose_name='Apellido')
     tel = models.CharField(max_length=10, null=False, verbose_name='Teléfono')
-    cargo = models.ForeignKey(Título, on_delete=models.CASCADE, blank=True, null=False)
+    titulos = models.ManyToManyField(Titulo, blank=False)
     def __str__(self):
         return '%s %s'%(self.apellido, self.nombre)  
     class Meta:
         db_table = 'Profesor'
         verbose_name = 'Profesor'
         verbose_name_plural = 'Profesores'
+
+class Tipo_de_Carrera(models.Model):
+    tipo = models.CharField(max_length=8, null=False, verbose_name='Tipo de carrera')
+    duracion = models.IntegerField(null=False, default=3, verbose_name='Duración')
+    def __str__(self):
+        return '%s %s'%(self.tipo, self.duracion)  
+    class Meta:
+        db_table = 'Tipo_Carrera'
+        verbose_name = 'Tipo de Carrera'
+        verbose_name_plural = 'Tipos de Carreras' 
+      
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=50, null=False, verbose_name='Nombre')
+    resolucion = models.CharField(max_length=40, null=False, verbose_name='Resolución')
+    def __str__(self):
+        return '%s '%(self.nombre)  
+    class Meta:
+        db_table = 'Carrera'
+        verbose_name = 'Carrera'
+        verbose_name_plural = 'Carreras'   
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=50, null=False, verbose_name='Nombre')
+    año = models.IntegerField(null=False, verbose_name='Año')
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE, blank=False)
+    def __str__(self):
+        return '%s '%(self.nombre)  
+    class Meta:
+        db_table = 'Materia'
+        verbose_name = 'Materia'
+        verbose_name_plural = 'Materias'      
+        
+class Alumno(models.Model):
+    dni = models.CharField(max_length=8, null=False, verbose_name='DNI')
+    nombre = models.CharField(max_length=20, null=False, verbose_name='Nombre')
+    apellido = models.CharField(max_length=20, null=False, verbose_name='Apellido')
+    tel = models.CharField(max_length=10, null=False, verbose_name='Teléfono')
+    carrera = models.ManyToManyField(Carrera, blank=False)
+    def __str__(self):
+        return '%s %s'%(self.apellido, self.nombre)  
+    class Meta:
+        db_table = 'Alumno'
+        verbose_name = 'Alumno'
+        verbose_name_plural = 'Alumnos'
